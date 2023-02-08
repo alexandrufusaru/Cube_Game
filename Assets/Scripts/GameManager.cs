37751f330dcd4ef183751c8a65ca9a22fbfa7b64
alexandrufusaru <124254197+alexandrufusaru@users.noreply.github.com>
2023-02-08 16:16:59 +0200

    Add files via upload

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement; // cand vrem sa ne jucam cu scenele(trecem de la una la alta, le restartam)
+
+public class GameManager : MonoBehaviour {
+
+    bool gameHasEnded = false;
+    public float restartDelay = 1f;
+    public GameObject completeLevelUI;
+    
+
+    public void CompleteLevel()
+    {
+        //  Debug.Log("Level WON");
+        completeLevelUI.SetActive(true);
+    }
+
+    public void EndGame()
+    {
+        if(gameHasEnded==false)
+        {
+            gameHasEnded = true;
+            Debug.Log("GAME OVER");
+
+            Invoke("Restart", restartDelay);
+        }
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody rb;
+
+    public float forwardForce;
+    public float sidewaysForce; // punem f la sfarsit doar la float
+	
+	// Update is called once per frame
+	void FixedUpdate () // folosim FixedUpdate cand facem modificari la nivel fizic(forte,gravitatie, etc)
+    {
+        // Time.deltaTime = nr de secunde de la ultimul frame
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        
+        if(Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if(rb.position.y<-1)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
